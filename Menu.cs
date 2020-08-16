0bd32911e3fb9dd8a37149a4d52add305d88030c
Iffy-A <48001160+Iffy-A@users.noreply.github.com>
2020-08-17 00:33:49 +0100

    Add files via upload

---
+++ Menu.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class Menu : MonoBehaviour {
+
+    public Button startButton;
+    public Button optionsButton;
+    public Button quitButton;
+
+    void playGame() { SceneManager.LoadScene(2); }
+    void options() { SceneManager.LoadScene(1); }
+    void quit() { Application.Quit(); }
+}
